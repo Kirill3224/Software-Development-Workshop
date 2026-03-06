9c94c244bd4db728cd1574af612c79cf8de4f38c
Kirill3224 <9217286@stud.kai.edu.ua>
2026-03-06 13:26:48 +0200

    feat(lab1): make program print matrix before and after main operation

--- lab1/MatrixProcessor.java
+++ lab1/MatrixProcessor.java
@@ -2,8 +2,8 @@ import java.util.Random;
 
 public class MatrixProcessor {
     public static void main(String[] args) {
-
-        System.out.println("Розробник: Ревякін Кирило Артемович");
+        
+        System.out.println("Розробник: Прізвище І.О.\n");
 
         int rows = 2;
         int cols = 3;
@@ -16,7 +16,28 @@ public class MatrixProcessor {
             }
         }
 
-        System.out.println("\n- Матриця А:");
+        System.out.println("- Матриця А (до обробки):");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(matrixA[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
+        double totalSum = 0;
+        int[] rowSums = new int[rows]; 
+
+        for (int i = 0; i < rows; i++) {
+            int currentRrowSum = 0;
+            for (int j = 0; j < cols; j++) {
+                currentRrowSum += matrixA[i][j];
+            }
+            rowSums[i] = currentRrowSum;
+            totalSum += currentRrowSum;
+        }
+        double average = totalSum / (rows * cols); 
+
+        System.out.println("\n- Матриця А (після обробки):");
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 System.out.print(matrixA[i][j] + "\t");
@@ -25,17 +46,10 @@ public class MatrixProcessor {
         }
 
         System.out.println("\n- Суми елементів матриці за рядками:");
-        double totalSum = 0;
         for (int i = 0; i < rows; i++) {
-            int rowSum = 0;
-            for (int j = 0; j < cols; j++) {
-                rowSum += matrixA[i][j];
-            }
-            totalSum += rowSum;
-            System.out.println("Рядок " + (i + 1) + ": " + rowSum);
+            System.out.println("Рядок " + (i + 1) + ": " + rowSums[i]);
         }
 
-        double average = totalSum / (rows * cols);
         System.out.printf("\n- Середнє арифметичне: %.2f\n", average);
     }
 }
