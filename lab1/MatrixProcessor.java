0f8aa731e4a59aaa479fbd5775d48ea62b243086
Kirill3224 <9217286@stud.kai.edu.ua>
2026-03-06 13:13:21 +0200

    feat(lab1): MatrixProcessor implemented

---
+++ lab1/MatrixProcessor.java
@@ -0,0 +1,41 @@
+import java.util.Random;
+
+public class MatrixProcessor {
+    public static void main(String[] args) {
+
+        System.out.println("Розробник: Ревякін Кирило Артемович");
+
+        int rows = 2;
+        int cols = 3;
+        int[][] matrixA = new int[rows][cols];
+        Random random = new Random();
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrixA[i][j] = random.nextInt(51);
+            }
+        }
+
+        System.out.println("\n- Матриця А:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(matrixA[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\n- Суми елементів матриці за рядками:");
+        double totalSum = 0;
+        for (int i = 0; i < rows; i++) {
+            int rowSum = 0;
+            for (int j = 0; j < cols; j++) {
+                rowSum += matrixA[i][j];
+            }
+            totalSum += rowSum;
+            System.out.println("Рядок " + (i + 1) + ": " + rowSum);
+        }
+
+        double average = totalSum / (rows * cols);
+        System.out.printf("\n- Середнє арифметичне: %.2f\n", average);
+    }
+}
